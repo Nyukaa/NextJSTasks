1779bcf2ebc17abc74a06b6907046c6e11e1d3c0
Nyukaa <annashitikova@gmail.com>
2026-05-05 17:27:09 +0300

    Task2 add About page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About this course</h1>
+      <p>
+        This course introduces the basics of Next.js, including routing,
+        components, styling, and server-side rendering.
+      </p>
+    </div>
+  );
+}
