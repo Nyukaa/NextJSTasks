7c15f871b6d153b0d44d5d5928d04d0f8d25d9b8
Nyukaa <annashitikova@gmail.com>
2026-05-05 17:48:41 +0300

    Task4 Update  content and style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,24 @@ import Link from "next/link";
 export default function About() {
   return (
     <div>
-      <h1>About this course</h1>
+      <h1> Hi, I am Anna — a full-stack developer with a curious mind</h1>
 
-      <p>
-        This course introduces Next.js basics like routing, pages, and
-        components.
+      <p
+        style={{
+          color: "#6a9a7a",
+
+          fontSize: "0.9rem",
+
+          margin: 0,
+        }}
+      >
+        Ever since Iwas a child, I dreamed of becoming a programmer. That dream
+        shaped my education: I studied mathematics and computer science,
+        building a solid foundation in logic and problem-solving. When I am not
+        coding, Iam probably playing piano, skiing through snowy trails, diving
+        into photography, or sipping a warm drink while sketching new ideas. I
+        believe in lifelong learning and enjoy working with people who value
+        clarity, creativity, and collaboration.
       </p>
 
       <Link href="/">Back to Home</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1
+        style={{
+          color: "#6a9a7f",
+
+          fontSize: "0.9rem",
+
+          margin: 0,
+        }}
+      >
+        Home Page
+      </h1>
 
       <Link href="/about">Go to About Page</Link>
     </div>
