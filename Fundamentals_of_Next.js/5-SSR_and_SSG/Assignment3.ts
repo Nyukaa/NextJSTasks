0d779ca84d1d2c1b86282cee24174c4da4d278a8
Nyukaa <annashitikova@gmail.com>
2026-05-08 17:32:53 +0300

    Task3 Add Profile page to fetch and display user data Server Rendered Profile

---
+++ Fundamentals_of_Next.js/5-SSR_and_SSG/Assignment3.ts
@@ -0,0 +1,3 @@
+// Assignment 3 Server Rendered Profile
+// Repository: https://github.com/Nyukaa/NextJSTasks
+// The implementation is in: app/profile/page.js
---
+++ app/profile/page.tsx
@@ -0,0 +1,23 @@
+export default async function Profile() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1", {
+    cache: "no-store", // Disable caching to always fetch fresh data
+  });
+
+  const user = await response.json();
+
+  return (
+    <div
+      style={{
+        fontSize: "18px",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Profile Page</h1>
+
+      <p>Welcome, {user.name}!</p>
+    </div>
+  );
+}
