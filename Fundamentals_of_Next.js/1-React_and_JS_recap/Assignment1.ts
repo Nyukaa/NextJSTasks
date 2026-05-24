3be5992c25f14acb1ea1658f4c7068578b3237a7
Nyukaa <annashitikova@gmail.com>
2026-05-24 11:11:25 +0300

    Add assignment

---
+++ Fundamentals_of_Next.js/1-React_and_JS_recap/Assignment1.ts
@@ -0,0 +1,2 @@
+// Assignment 1
+// Repository: https://github.com/Nyukaa/NextJSTasks
---
+++ Fundamentals_of_Next.js/8-Deployment_and_Optimization/Assignment3.ts
@@ -0,0 +1,5 @@
+// Assignment 3 Add a Third-Party Script with
+// Repository: https://github.com/Nyukaa/NextJSTasks
+// The implementation is in: app/page.tsx
+
+//Completed assignments in this chapter: 1-3 (3/3)
